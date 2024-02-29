26a2445b7e7ff9c66b9b8f574f91f2c155f73018
Chxshire22 <akuindra22@gmail.com>
2024-02-29 17:01:11 +0800

    validation for form done.

--- src/app/prototyping/page.jsx
+++ src/app/prototyping/page.jsx
@@ -2,20 +2,34 @@
 import { CardImage } from "react-bootstrap-icons";
 import { useRouter } from "next/navigation";
 import PageHeaderWithBackBtn from "@/components/PageHeaderWithBackBtn";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function CreateThreadsPage() {
-  const [preview, setPreview] = useState(null)
-  const [selectedImage, setSelectedImage] = useState(null)
+  const [preview, setPreview] = useState(null);
+
+  const [threadData, setThreadData] = useState({
+    selectedImage: null,
+    title: "",
+    startDate: null,
+    endDate: null,
+    country: "",
+  });
+
+  useEffect(() => {
+    console.log(threadData);
+  }, [threadData]);
   const router = useRouter();
 
   const handleImageChange = (e) => {
-    		if (!e.target.files || e.target.files.length === 0) {
-          setSelectedImage(null);
-          return;
-        }
-        setSelectedImage(e.target.files[0]);
-        setPreview(URL.createObjectURL(e.target.files[0]));
+    if (!e.target.files || e.target.files.length === 0) {
+      setSelectedImage(null);
+      return;
+    }
+    setThreadData((prevState) => ({
+      ...prevState,
+      selectedImage: e.target.files[0],
+    }));
+    setPreview(URL.createObjectURL(e.target.files[0]));
   };
 
   return (
@@ -39,8 +53,19 @@ export default function CreateThreadsPage() {
 
       {/* Select country */}
       <div className="my-3">
-        <select className="form-select" aria-label="Country">
-          <option defaultValue={null}>Select country</option>
+        <select
+          className="form-select"
+          aria-label="Country"
+          onChange={(e) => {
+            setThreadData((prevState) => ({
+              ...prevState,
+              country: e.target.value,
+            }));
+          }}
+        >
+          <option defaultValue={null} value="0">
+            Select country
+          </option>
           <option value="1">Map</option>
           <option value="2">Countries</option>
           <option value="3">Here</option>
@@ -53,6 +78,12 @@ export default function CreateThreadsPage() {
           className="form-control"
           id="titleInput"
           placeholder="Title"
+          onChange={(e) => {
+            setThreadData((prevState) => ({
+              ...prevState,
+              title: e.target.value,
+            }));
+          }}
         />
         <label className="form-label" htmlFor="floatingPassword">
           Title
@@ -61,16 +92,36 @@ export default function CreateThreadsPage() {
       {/* Input Dates */}
       <div className="start-end-input">
         <div className="form-floating mb-3 date-input">
-          <input type="date" className="form-control" id="startDateInput" />
+          <input
+            type="date"
+            className="form-control"
+            id="startDateInput"
+            onChange={(e) => {
+              setThreadData((prevState) => ({
+                ...prevState,
+                startDate: e.target.value,
+              }));
+            }}
+          />
           <label htmlFor="startDateInput">End date</label>
         </div>
         <div className="form-floating mb-3 date-input">
-          <input type="date" className="form-control" id="endDateInput" />
+          <input
+            type="date"
+            className="form-control"
+            id="endDateInput"
+            onChange={(e) => {
+              setThreadData((prevState) => ({
+                ...prevState,
+                endDate: e.target.value,
+              }));
+            }}
+          />
           <label htmlFor="endDateInput">End date</label>
         </div>
       </div>
 
-      <button type="submit" className="btn btn-submit-form">
+      <button type="submit" disabled={threadData.country == ""||threadData.title == "" || threadData.selectedImage ==null||threadData.startDate ==null||threadData.endDate == null? true: false} className="btn btn-submit-form">
         Post
       </button>
       {/* spacer is needed as navbar is sticky nad will block out the lowest elements on page */}
